422720dcdd4137195fd8cec04fed331460b91fb0
João Fernando <joaofernando@DESKTOP-9E4H3O7.localdomain>
2021-05-22 09:15:20 -0300

    accordion add e mainFoto na galeria

---
+++ admin/src/containers/AuthPage/components/Logo/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import LogoErrera from "../../../../assets/images/logo-errera-preto.png";
+import Img from "./Img";
+
+const Logo = () => <Img src={LogoErrera} alt="errera-logo" />;
+
+export default Logo;
