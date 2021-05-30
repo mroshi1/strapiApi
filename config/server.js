5ccd491719e7ec1586e4c2419fb5f07e7e8f3013
Vitor Hugo <vitor@VITOR-PC.localdomain>
2021-05-30 14:11:55 -0300

    Mudanças na HomePage e no botão da AuthPage

--- config/server.js
+++ config/server.js
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 5500),
+  port: env.int('PORT', 1337),
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', 'dde6748f3b3cd2aac2498cbc3e83087f'),
